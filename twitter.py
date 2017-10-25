bd33a62b26eefb1183cb23ba2ae576651f1b6328
Vineeth Reddy <vineethreddy2197@gmail.com>
2017-10-25 21:12:14 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -15,10 +15,10 @@ def login_to_twitter(consumer_key, consumer_secret, access_token, access_token_s
  
 def post_tweets():
  
-    consumer_key	   =   '4aHcHBGMhnLHIL0qb7JuCbriM'
-    consumer_secret	   =   'MzAUvilX2VA5DKKdV9ZtP5j1D8vK3NkYbKomvsr3ikwPdYvaSD'
-    access_token	   =   '3235188956-o95DXCGws2sinldAQxCcIBk25joU1oYix4xsA2L'
-    access_token_secret   =   'ZRd0qy70vqvtXtv4TCnYWauspVP2GHd4nayhkAquzlvid'
+    consumer_key	   =   ''
+    consumer_secret	   =   ''
+    access_token	   =   ''
+    access_token_secret   =   ''
  
     message = "LOL..! OK!"
  
